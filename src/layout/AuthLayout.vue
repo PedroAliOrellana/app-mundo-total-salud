d0698867904ffdda503704f27b367409d3dc3b9e
PedroAliOrellana <pedroaliorellana@gmail.com>
2021-01-08 16:26:15 -0400

    Update AuthLayout.vue

--- src/layout/AuthLayout.vue
+++ src/layout/AuthLayout.vue
@@ -5,7 +5,7 @@
                   containerClasses="px-4 container"
                   expand>
             <router-link slot="brand" class="navbar-brand" to="/">
-                <img src="img/brand/total_salud.jpg"/>
+                <img src="img/brand/Total_Salud.jpg"/>
             </router-link>
 
             <template v-slot="{closeMenu}">
